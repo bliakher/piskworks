c7a57d3dda357956d86b82af81d9a1bce772cc6f
Evgenia Golubeva <evgenia.m.golubeva@gmail.com>
2021-08-31 12:03:52 +0200

    add color when loading

--- Graphics/Model3DLoader.cs
+++ Graphics/Model3DLoader.cs
@@ -33,28 +33,22 @@ namespace piskworks
     
     public class Model3DLoader
     {
-        private string _sourceFileName;
         private GraphicsDevice _graphicsDevice;
 
-        public Model3DLoader(string sourceFileName, GraphicsDevice graphicsDevice)
+        public Model3DLoader(GraphicsDevice graphicsDevice)
         {
-            _sourceFileName = sourceFileName;
             _graphicsDevice = graphicsDevice;
         }
 
-        public List<Model3D> Load()
+        public Model3D Load(string sourceFileName)
         {
-            var models = new List<Model3D>();
-            var reader = new StreamReader(_sourceFileName);
+            var reader = new StreamReader(sourceFileName);
             var line = reader.ReadLine();
             List<VertexPosition> vertices = null;
             List<int> indices = null;
             while (line != null) {
                 var segments = line.Split();
                 if (segments[0] == "o") { // beginning of new model
-                    if (vertices != null && indices != null) {
-                        models.Add(new Model3D(_graphicsDevice, vertices, indices));
-                    }
                     vertices = new List<VertexPosition>();
                     indices = new List<int>();
                 }
@@ -67,19 +61,49 @@ namespace piskworks
                 else if (segments[0] == "f") { // face - 3 vertex indexes - f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
                     for (int i = 1; i < 4; i++) {
                         var indexTriplet = segments[i].Split("/");
-                        var index = Int32.Parse(indexTriplet[0]);
+                        var index = Int32.Parse(indexTriplet[0]) - 1; // in .obj indexes start at 1
                         indices.Add(index);
                     }
                 }
                 
                 line = reader.ReadLine();
             }
-            if (vertices.Count > 0) {
-                models.Add(new Model3D(_graphicsDevice, vertices, indices));
+            
+            reader.Close();
+            return new Model3D(_graphicsDevice, vertices, indices);
+        }
+        
+        public Model3D LoadWithColor(string sourceFileName, Color color)
+        {
+            var reader = new StreamReader(sourceFileName);
+            var line = reader.ReadLine();
+            List<VertexPositionColor> vertices = null;
+            List<int> indices = null;
+            while (line != null) {
+                var segments = line.Split();
+                if (segments[0] == "o") { // beginning of new model
+                    vertices = new List<VertexPositionColor>();
+                    indices = new List<int>();
+                }
+                else if (segments[0] == "v") { // vertex - v 0.123 0.234 0.345
+                    var x = float.Parse(segments[1]);
+                    var y = float.Parse(segments[2]);
+                    var z = float.Parse(segments[3]);
+                    vertices.Add(new VertexPositionColor(new Vector3(x, y, z), color));
+                }
+                else if (segments[0] == "f") { // face - 3 vertex indexes - f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
+                    for (int i = 1; i < 4; i++) {
+                        var indexTriplet = segments[i].Split("/");
+                        var index = Int32.Parse(indexTriplet[0]) - 1; // in .obj indexes start at 1
+                        indices.Add(index);
+                    }
+                }
+                
+                line = reader.ReadLine();
             }
             
             reader.Close();
-            return models;
+            return new Model3D(_graphicsDevice, vertices, indices);
         }
     }
 }
